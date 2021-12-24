f8b24b5ebf0a79941f97edd9356cd68be73143fc
SufGtps <78003730+SufGtps@users.noreply.github.com>
2021-12-25 04:45:52 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -22,8 +22,8 @@ var lolkill = db.get("lolkill");
 	console.log('')
 }
 
-var creator = 'ZeeoneOfc' // ubah jadi nama lu
-const listkey = ["Alphabot","Zeeone","ZeeoneOfc"]; // ubah apikey nya, tambah aja klo mau
+var creator = 'SufDev' // ubah jadi nama lu
+const listkey = ["sufdev","sufbot","scarg"]; // ubah apikey nya, tambah aja klo mau
 
 var axios = require('axios');
 var qs = require('qs');
